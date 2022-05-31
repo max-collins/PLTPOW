d2037962b7c7aea8518b630e4cf6c925627844de
max_collins_ <35351270+max-collins@users.noreply.github.com>
2022-05-31 16:52:54 -0700

    changed the evaluator classes inputs

--- variance/variance_prediction_funcs.py
+++ variance/variance_prediction_funcs.py
@@ -8,14 +8,30 @@ class Evaluator:
     Class for evaluating the preformance of manually made models. Input the variance you want to test for
     """
     def __init__(self, variance = None) -> None:
+        self.variance = variance
         if type(variance) != None:
             self.df = df.loc[df['Description'] == variance]
         else:
             self.df = df
     
-    def accuracy_score(self, predictions, y):
+    def accuracy_score(self, model, params):
+        df_roll = df.loc[df['Description'] == 'Rollish']
+        df_low = df.loc[df['Description'] == 'Low']
+        df_med = df.loc[df['Description'] == 'Medium']
+        df_high = df.loc[df['Description'] == 'High']
+        n = min(df_roll.shape[0], df_low.shape[0], df_med.shape[0], df_high.shape[0])
+        df_roll = df.loc[df['Description'] == 'Rollish'].sample(n = n)
+        df_low = df.loc[df['Description'] == 'Low'].sample(n = n)
+        df_med = df.loc[df['Description'] == 'Medium'].sample(n = n)
+        df_high = df.loc[df['Description'] == 'High'].sample(n = n)
+        df_eval = pd.concat([df_roll, df_low, df_med, df_high])
+
+        X = df_eval[params]
+        y = df_eval[self.variance]
+        predictions = model(X)
         current_correct = 0 
-        for i,prediction in np.ndenumerate(predictions):
+        for i in range(predictions.shape[0]):
+            prediction = predictions[i]
             if prediction == y.iloc[i]:
                 current_correct += 1
         return current_correct/len(y)
@@ -23,27 +39,49 @@ class Evaluator:
     def dummy_score(self):
         high_rows = df[df['High'] == True]
         percentage = high_rows.shape[0]/df.shape[0]
+        return percentage
         
-
+########################## high variance  ##############################
 def high_prediction(X):
     """
     manual made model for predicting when a position is high or not high
     """
     predictions = np.array([])
-    for x in np.nditer(X):
-        if -2*x + 1 >= 0:
+    for i, x in np.ndenumerate(X):
+        if -2.7*x + 1 >= 0:
             predictions = np.append(predictions, [True])
         else: 
             predictions = np.append(predictions, [False])
     return predictions
 
-def high_pred_easy(X):
+def high_pred_hard(X):
     """
-    Attempted simplification of high_model
+    better_high_model
     """
     predictions = np.array([])
-    for x in np.nditer(X):
-        if x <= 1:
+    for i in range(X.shape[0]):
+        data_point = X.iloc[i]
+        ad = data_point[0]
+        three = data_point[1]
+        if -3*ad + -1*three + 4 >= 0:
+            predictions = np.append(predictions, [True])
+        else:
+            predictions = np.append(predictions, [False])
+    return predictions
+#####################################################################
+
+
+###################### rollish variance #############################
+def roll_pred(X):
+    """
+    rollish model
+    """
+    predictions = np.array([])
+    for i in range(X.shape[0]):
+        data_point = X.iloc[i]
+        ad = data_point[0]
+        pc = data_point[1]
+        if .6*ad + -.1*pc -1.5 >= 0:
             predictions = np.append(predictions, [True])
         else:
             predictions = np.append(predictions, [False])
